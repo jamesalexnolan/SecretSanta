004b1fa446fee3954c668f31c65938a14e9ab551
James nolan <jamesnolan1@hotmail.com>
2021-10-20 22:42:18 +0100

    homepage css

--- src/src/views/Home.vue
+++ src/src/views/Home.vue
@@ -1,29 +1,61 @@
 <template>
-  <div class="home">
-  
-     <b-container>
-        <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-          <p>For more information visit our website</p>
-          <b-btn variant="primary" href="https://bootstrap-vue.org/">More Info</b-btn>
-        </b-jumbotron>
-        </b-container>
-<h1>Okay</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6 create-column d-flex align-items-center justify-content-center">
+        <button class="btn btn-lg active shadow-sm p-3 mb-5 bg-white"><b-icon variant="success" class="mr-2" icon="tree-fill"></b-icon>Create a Secret Santa</button>
+      </div>
+      <div class="col-6 manage-column d-flex align-items-center justify-content-center">
+        <button class="btn btn-lg active shadow-sm p-3 mb-5 bg-white"><b-icon variant="danger" class="mr-2" icon="gift-fill"></b-icon>Manage a Secret Santa</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
 
 <style scoped lang="scss">
-$color:red;
+.create-column {
+  &:after {
+    content: "";
+    background: url("~@/assets/christmas-bulb.jpg");
+    opacity: 0.5;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    z-index: -1;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  min-height: 500px;
+  position: relative;
+}
 
-.home{
-  background: $color
+.manage-column {
+  &:after {
+    content: "";
+    background: url("~@/assets/christmas-tree.jpg");
+    opacity: 0.5;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    z-index: -1;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  min-height: 500px;
+  position: relative;
 }
 </style>
